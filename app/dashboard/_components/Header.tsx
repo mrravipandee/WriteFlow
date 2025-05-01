d6e6adf1d13833c46213a0b9dbfc8a5c7d8f52de
Ravi Pandey <imravipanday@gmail.com>
2025-05-01 16:37:20 +0530

    dashboard manubar

---
+++ app/dashboard/_components/Header.tsx
@@ -0,0 +1,22 @@
+import { Search } from 'lucide-react'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='p-5 shadow-sm border-b-2 flex justify-between'>
+      <div className="flex gap-2 items-center p-2 border rounded-md max-w-lg item-center">
+        <Search />
+        <input
+          type="text"
+          placeholder="Search..."
+          className='outline-none'
+        />
+      </div>
+      <div className=''>
+        <h2 className='bg-primary p-2 rounded-full text-xs text-center text-white px-2'>Join Membership jsut for $9.99/Month</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ app/dashboard/_components/SideNav.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { FolderClock, Home, Settings, Wallet } from 'lucide-react'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const SideNav = () => {
+  const pathname = usePathname()
+
+  const menuItems = [
+    { name: 'Home', icon: Home, path: '/dashboard' },
+    { name: 'History', icon: FolderClock, path: '/dashboard/history' },
+    { name: 'Billing', icon: Wallet, path: '/dashboard/billing' },
+    { name: 'Setting', icon: Settings, path: '/dashboard/setting' },
+  ]
+
+  return (
+    <div className='h-screen p-5 shadow-sm border'>
+      <div className='flex justify-center'>
+        <Image
+          src={'/logo.svg'}
+          alt='logo'
+          width={100}
+          height={100}
+          className='mx-auto mt-5'
+        />
+      </div>
+      <hr className='my-6 border' />
+
+      <div className='mt-3'>
+        {menuItems.map((item, index) => (
+          <div
+            key={index}
+            className={`flex gap-2 mb-2 p-2 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${pathname === item.path ? 'bg-primary text-white' : ''
+              }`}
+          >
+            <item.icon />
+            <h2>{item.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SideNav
