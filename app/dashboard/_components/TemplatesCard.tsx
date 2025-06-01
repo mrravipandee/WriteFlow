9f5cc117ea99c1ecfd3f7e259f73ee480142ecb0
Ravi Pandey <imravipanday@gmail.com>
2025-06-02 01:32:44 +0530

    'use client'
    import Image from 'next/image';
    import Link from 'next/link';
    import React from 'react';
    import { TEMPLATE } from './TemplateTools';
    
    const TemplatesCard: React.FC<TEMPLATE> = ({ slug, icon, name, desc }) => {
      return (
        <Link href={}>
          <div className='p-5 shadow-md rounded-md border bg-white flex flex-col gap-2 cursor-pointer hover:scale-105 hover:transition-all'>
            <Image src={icon} alt='icon' width={50} height={50} />
            <h2 className='font-medium text-lg'>{name}</h2>
            <p className='text-gray-500 line-clamp-3'>{desc}</p>
          </div>
        </Link>
      );
    };
    
    export default TemplatesCard;

--- app/dashboard/_components/TemplatesCard.tsx
+++ app/dashboard/_components/TemplatesCard.tsx
@@ -1,18 +1,20 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { TEMPLATE } from './TemplateTools'
+'use client'
 
-const TemplatesCard = (tools: TEMPLATE, key: number) => {
-    return (
-        <Link href={`/dashboard/content/${tools?.slug}`} key={key}>
-            <div className='p-5 shadow-md rounded-md border bg-white flex flex-col gap-2 cursor-pointer hover:scale-105 hover:transition-all'>
-                <Image src={tools.icon} alt='icon' width={50} height={50} />
-                <h2 className='font-medium text-lg'>{tools.name}</h2>
-                <p className='text-gray-500 line-clamp-3'>{tools.desc}</p>
-            </div>
-        </Link>
-    )
-}
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { TEMPLATE } from './TemplateTools';
+
+const TemplatesCard: React.FC<TEMPLATE> = ({ slug, icon, name, desc }) => {
+  return (
+    <Link href={`/dashboard/content/${slug}`}>
+      <div className='p-5 shadow-md rounded-md border bg-white flex flex-col gap-2 cursor-pointer hover:scale-105 hover:transition-all'>
+        <Image src={icon} alt='icon' width={50} height={50} />
+        <h2 className='font-medium text-lg'>{name}</h2>
+        <p className='text-gray-500 line-clamp-3'>{desc}</p>
+      </div>
+    </Link>
+  );
+};
 
 export default TemplatesCard;
